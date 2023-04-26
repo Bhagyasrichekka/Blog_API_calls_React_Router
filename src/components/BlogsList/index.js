46aa53a46c5057934ef280f8299c622c89693de8
Bhagyasrichekka <chekkabhagyasree4gmail.com>
2023-04-26 10:24:41 +0000

    API calls and React Router

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,43 @@
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import {Component} from 'react'
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount = () => {
+    this.getBlogsList()
+  }
+
+  getBlogsList = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      title: eachItem.title,
+      author: eachItem.author,
+    }))
+    this.setState({blogsData: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
